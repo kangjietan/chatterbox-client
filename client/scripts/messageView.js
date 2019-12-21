c3a64632969af33fd960fe570dfa5cb30c8ddcaf
Hack Reactor Student <student@hackreactor.com>
2019-12-21 15:58:13 -0800

    Tried to add friend functionality

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -2,10 +2,24 @@ var MessageView = {
 
   render: _.template(`
       <div class="chat">
-        <div class="username"><%- this.username %>:</div>
+        <a href=javascript: highlight('" + <%- this.username %> + "'); > <%- this.username %> </a>
+        <div class="username"><%- this.username %></div>
         <div class="<%- this.roomname %>"></div>
         <div class="text"><%- this.text %></div>
       </div>
-    `)
+    `),
 
+  // highlight: function (name) {
+  //   var friends = document.getElementsByClassName(name);
+  //   friends.forEach( (friend) => {
+  //     friend.css("color", "red");
+  //   });
+  // }
+};
+
+var highlight = function (name) {
+  var friends = document.getElementsByClassName(name);
+  friends.forEach( (friend) => {
+    friend.css("color", "red");
+  });
 };
